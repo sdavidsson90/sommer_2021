cf6d2525b797e37304a339051922b7897ddf93f1
Laurbergknudsen <laurberg_knudsen@hotmail.com>
2021-05-26 16:01:23 +0200

    Region tilføjet korrekt + usmaps

--- test_til_mdl.R
+++ test_til_mdl.R
@@ -1,11 +1,24 @@
 pacman::p_load(readr,tidyverse,RSQLite,utils,waldo,
-               svDialogs,DescTools,zoo,textclean, rvest)
+               svDialogs,DescTools,zoo,textclean, rvest, usmap)
 
 if (!exists("final_data_2")) {
   donorsChoose <- dbConnect(RSQLite::SQLite(), "SQLdatabase/donorsChoose.sqlite")
   final_data_2 <- dbGetQuery(donorsChoose, 'SELECT * FROM final_data_2')
   dbDisconnect(donorsChoose)
 }
+
+url = 'https://raw.githubusercontent.com/cphalpert/census-regions/master/us%20census%20bureau%20regions%20and%20divisions.csv'
+
+
+regions <- read_csv(url(url))
+
+regions <- regions %>% 
+  mutate(
+    state = tolower(State)
+  ) %>% 
+  rename(state_code =`State Code`) %>% 
+  select(-State)
+
 
 
 url = 'https://en.m.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States'
@@ -22,29 +35,10 @@ table_list <- url %>%
            state = tolower(`State orfederal district`))   %>% 
     select(state,længdegrad,breddegrad) )
 
-
-url = 'https://www.nationsonline.org/oneworld/US-states-by-area.htm'
-
-region_table <- url %>%
-  read_html() %>% 
-  html_nodes("table") %>%
-  html_table() %>% .[[3]]
-
-region_table <- region_table[,c(3,7)] %>% 
-  mutate(
-    state = tolower(State),
-    region = `Census Region`
-  ) %>% 
-  select(state,region)
+states <- left_join(regions,states_table)
 
 
-states <- left_join(states_table,region_table)
-
-states <- states %>% 
-  arrange(længdegrad) %>% 
-  mutate(region = ifelse(is.na(region)& lag(region,1) == lead(region,1),lead(region,1),region)) %>% 
-  arrange(state)
-
+complete.cases(states)
 
 navne_filter = c("donation_included_optional_donation",
                  "donation_amount", "project_type", "project_grade_level_category",
@@ -56,12 +50,18 @@ navne_filter = c("donation_included_optional_donation",
 a <- final_data_2 %>% 
   select(all_of(navne_filter)) %>% 
   mutate_if(is.character,tolower) %>% 
-  left_join(states[,c(1,4)], by=c("donor_state" = "state")) %>% 
-  rename(donor_region = region) %>% 
-  left_join(states[,c(1,4)], by=c("school_state" = "state")) %>% 
-  rename(school_region = region) %>% 
+  left_join(states[,c(2,4)], by=c("donor_state" = "state")) %>% 
+  rename(donor_region = Region) %>% 
+  left_join(states[,c(2,4)], by=c("school_state" = "state")) %>% 
+  rename(school_region = Region) %>% 
   mutate(
     same_region= ifelse(donor_region == school_region,TRUE,FALSE),
     same_state = ifelse(donor_state == school_state,TRUE,FALSE)
   ) %>% 
-  mutate_if(is.character,as.factor)
+  mutate_if(is.character,as.factor) %>% 
+  as_tibble()
+
+plot_usmap(regions = "states", data = states, values = "Region") + 
+  theme(legend.position = "right") +
+  scale_fill_discrete(name = "Region")
+
